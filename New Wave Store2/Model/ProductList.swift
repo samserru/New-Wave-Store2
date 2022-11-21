3ba1877cb1c6bd69af4cd52193406d67a6c05683
Sam Serruya <sammyyrap7@gmail.com>
2022-11-21 08:27:45 -0500

    updated

--- New Wave Store2/Model/ProductList.swift
+++ New Wave Store2/Model/ProductList.swift
@@ -8,6 +8,10 @@
 import Foundation
 
 class ProductList{
+  
+    @Published var products: [Product]
     
-    var products = [Product(name: "NW-PC",description: "New Wave's worst computer", image: "NW-PC", quantity: 100, price: 9999.99),Product(name: "wPhone",description: "New Wave's worst computer", image: "wPhone", quantity: 200, price: 69.3),Product(name: "wPen",description: "New Wave's worst computer", image: "wPen", quantity: 300, price: 10.8),Product(name: "software",description: "New Wave's worst computer", image: "software", quantity: 250, price: 1000)]
+    init(){
+    products = [Product(name: "NW-PC",description: "New Wave's worst computer", image: "NW-PC", quantity: 100, price: 9999.99),Product(name: "wPhone",description: "New Wave's worst computer", image: "wPhone", quantity: 200, price: 69.3),Product(name: "wPen",description: "New Wave's worst computer", image: "wPen", quantity: 300, price: 10.8),Product(name: "software",description: "New Wave's worst computer", image: "software", quantity: 250, price: 1000)]
+    }
 }
