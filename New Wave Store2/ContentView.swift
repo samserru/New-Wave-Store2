91513bd7735cf74c5313e39ef2fcf5da3df585a7
Sam Serruya <sammyyrap7@gmail.com>
2022-11-21 07:37:07 -0500

    Initial Commit

---
+++ New Wave Store2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  New Wave Store2
+//
+//  Created by Samuel Serruya (student LM) on 11/21/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ New Wave Store2/New_Wave_Store2App.swift
@@ -0,0 +1,17 @@
+//
+//  New_Wave_Store2App.swift
+//  New Wave Store2
+//
+//  Created by Samuel Serruya (student LM) on 11/21/22.
+//
+
+import SwiftUI
+
+@main
+struct New_Wave_Store2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
