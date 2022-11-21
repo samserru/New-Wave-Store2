7d4b63da4327616951a15da238a507a59f623259
Sam Serruya <sammyyrap7@gmail.com>
2022-11-21 08:07:00 -0500

    added images, icon set, and product class

---
+++ New Wave Store2/Model/Product.swift
@@ -0,0 +1,25 @@
+//
+//  Product.swift
+//  New Wave Store2
+//
+//  Created by Samuel Serruya (student LM) on 11/21/22.
+//
+
+import Foundation
+
+class Product{
+    
+    var name : String
+    var description : String
+    var image : String
+    var quantity : Int
+    var price : Double
+    
+    init(name:String = "NW-PC", description:String = "NW-PC", image:String = "NW-PC", quantity:Int = 100, price:Double = 9999.99){
+        self.name=name
+        self.description=description
+        self.image=image
+        self.quantity=quantity
+        self.price=price
+    }
+}
